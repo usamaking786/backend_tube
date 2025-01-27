6f4be662bd739eb1306590c4a21a36f8b9fffc1c
usamaking786 <usamaahmadansari@gmail.com>
2025-01-27 21:54:00 +0500

    userWatchHistory aggregation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -389,8 +389,63 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 
 })
 
+const getUserWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },{
+            $lookup:{
+                from :"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from :"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch History Fetched Successfully"
+            )
+    )
+
+})
+
 export {
     registerUser,
+    getUserWatchHistory,
     loginUser,
     logoutUser,
     refreshAccessToken,
@@ -399,6 +454,7 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { 
+    changeCurrentPassword,
+     getCurrentUser,
+     getUserChannelProfile, 
+     getUserWatchHistory,  
+     loginUser, 
+     logoutUser, 
+     refreshAccessToken, 
+     registerUser, 
+     updateAccountDetails, 
+     updateUserAvatar, 
+     updateUserCoverImage 
+    } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -21,6 +33,15 @@ router.route("/register").post(
     
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt,logoutUser);
+router.route("/refresh-token").post(verifyJwt,refreshAccessToken);
+router.route("/change-password").post(verifyJwt,changeCurrentPassword);
+router.route("/current-user").get(verifyJwt,getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"),updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt,getUserWatchHistory);
+
 
 
 export default router;
